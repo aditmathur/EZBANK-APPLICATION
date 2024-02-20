c00cfb5632d785fc2d0d1a8d21960b506ca84621
Adit Mathur <aditmathur7@gmail.com>
2024-02-21 00:59:15 +0530

    Actuator refresh config endpoint exposed

--- cards/src/main/java/com/ezbank/cards/dto/CardsContactInfoDTO.java
+++ cards/src/main/java/com/ezbank/cards/dto/CardsContactInfoDTO.java
@@ -1,10 +1,20 @@
 package com.ezbank.cards.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class CardsContactInfoDTO {
+
+    String message;
+
+    Map<String, String> contactDetails;
+
+    List<String> onCallSupport;
 }
